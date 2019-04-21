b1a127ef774a6c8196fe82ed7bdfd0df4be5f363
anton_br <ziliboba753@gmail.com>
2019-04-21 13:23:35 +0300

    added distance metric

--- CNN/preprocess.py
+++ CNN/preprocess.py
@@ -1,13 +1,33 @@
 """File with torch dataset instance and functions for preprocessing."""
 import os
 
-import torch
 import numpy as np
+from sklearn.neighbors import BallTree
+from scipy.spatial import distance as sc_distance
 
-from torchvision import transforms
+import torch
 from torch.utils.data import Dataset
 
 
+def calculate_distance(true_coords_vector, coords_vector):
+    if len(coords_vector) == 0 and len(true_coords_vector) != 0:
+        res = 0
+        return np.sum(sc_distance.cdist(np.array([[100, 100]]), true_coords_vector,
+                                        'euclidean')) + 16*len(true_coords_vector)
+    if len(coords_vector) != 0 and len(true_coords_vector) == 0:
+        return np.sum(sc_distance.cdist(np.array([[100, 100]]), coords_vector,
+                                        'euclidean')) + 16*len(coords_vector)
+    if len(coords_vector) == 0 and len(true_coords_vector) == 0:
+        return 0
+    true_coords = np.vstack(tuple({tuple(row) for row in true_coords_vector}))
+    coords = np.vstack(tuple({tuple(row) for row in coords_vector}))
+    tree_true_coords = BallTree(true_coords)
+    tree_coords = BallTree(coords)
+    distance_from_true_array, _ = tree_true_coords.query(coords)
+    distance_from_found_array, _ = tree_coords.query(true_coords)
+    distance_from_found = np.sum(distance_from_found_array)
+    distance_from_true = np.sum(distance_from_true_array)
+    return np.sum(distance_from_found_array) + np.sum(distance_from_true_array)
 
 class FermiDataset(Dataset):
 
@@ -16,19 +36,19 @@ class FermiDataset(Dataset):
         self.points_path = points_path
         self.index = sorted(os.listdir(self.images_path))
         self.transform = transform
-        
+
     def __len__(self):
         return len(self.index)
 
     def create_target(self, y):
         mask = np.zeros((200, 200), dtype=np.int32)
         for coord in y:
-            mask[coord[0], coord[1]]=1
+            mask[coord[0], coord[1]] = 1
         return np.array(mask, dtype=np.int64)
 
 
     def __getitem__(self, idx):
-        
+
         image = np.load(os.path.join(self.images_path, self.index[idx]))
         image = image.reshape(*image.shape, -1)
         points = np.load(os.path.join(self.points_path, self.index[idx]))
@@ -54,6 +74,8 @@ class RandomCrop(object):
         h, w = image.shape[:2]
         new_h, new_w = self.output_size
 
+#         points = np.array(np.where(sample[1])).T
+#         if np.random.random > .5 or len(points[0])==0:
         top = np.random.randint(0, h - new_h)
         left = np.random.randint(0, w - new_w)
 
@@ -64,6 +86,39 @@ class RandomCrop(object):
                     left: left + new_w]
 
         return tuple([image, mask])
+
+class RandomCropNearPoints(object):
+    #doesnt work right now
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+
+    def __call__(self, sample):
+        image, mask = sample[0], sample[1]
+
+        h, w = image.shape[:2]
+        new_h, new_w = self.output_size
+
+
+        points = np.array(np.where(sample[1])).T
+
+        # crop near to answer with probability .5.
+
+        if np.random.random > .5 or len(points[0]) == 0:
+            #usiual crop
+            top = np.random.randint(0, h - new_h)
+            left = np.random.randint(0, w - new_w)
+        else:
+            #crop near random point
+            sample_point = np.random.choice(points)
+            left = np.random.randint(0, h - sample_point[0])
+            right = np.random.randint(0, w - sample_point[1])
+
+        return None
 
 class OneHotEncoding(object):
     def __init__(self, num_classes):
@@ -81,6 +136,5 @@ class ToTensor(object):
     def __call__(self, sample):
         image, mask = sample[0], sample[1]
         image = image.transpose((2, 0, 1))
-        mask = mask.transpose((2, 0, 1))
         return tuple([torch.from_numpy(image).to(torch.float32),
                       torch.from_numpy(mask).to(torch.float32)])
